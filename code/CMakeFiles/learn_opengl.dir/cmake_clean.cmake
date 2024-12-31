020ff649e212f961beb3e152e9c0e02151e250b6
Evelyn Hill <evelynplumhill@gmail.com>
2024-12-30 20:02:37 -0500

    Added make stuffs and got basic GLFW + GLAD context/windowing stuffs in

---
+++ code/CMakeFiles/learn_opengl.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn_opengl.dir/src/glad.c.o"
+  "CMakeFiles/learn_opengl.dir/src/glad.c.o.d"
+  "CMakeFiles/learn_opengl.dir/src/main.cpp.o"
+  "CMakeFiles/learn_opengl.dir/src/main.cpp.o.d"
+  "learn_opengl"
+  "learn_opengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/learn_opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ notes/creating-a-window.md
@@ -0,0 +1,10 @@
+## Creating a window
+
+The first step before creating graphics is creating an OpenGL context and
+a window to draw that context in. OpenGL purposefully abstracts itself from The
+window and context creation process because it is platform dependent.
+
+
+## GLFW
+is a library that allows us to create an openGL context and window on any
+platform.
